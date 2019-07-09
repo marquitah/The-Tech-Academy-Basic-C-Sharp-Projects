6a0a6ffe14e49f10d23cb2a8dd6b865e89647db5
Marquita <Marquita.dec@gmail.com>
2019-07-09 12:24:43 -0600

    Add files via upload

---
+++ CarInsuranceBooleanLogic/CarInsuranceBooleanLogic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace CarInsuranceBooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string age;
+            Console.WriteLine("What is your age?");
+            age = Console.ReadLine();
+            int age1 = Convert.ToInt32(age);
+
+            string DUI;
+            Console.WriteLine("Have you ever had a DUI? Enter \"yes\" or \"no\".");
+            DUI = Console.ReadLine();
+
+            string speedingTickets;
+            Console.WriteLine("How many speeding tickets do you have?");
+            speedingTickets = Console.ReadLine();
+            int speedingTickets1 = Convert.ToInt32(speedingTickets);
+
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age1 > 15 && DUI == "no" && speedingTickets1 <= 3);
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
