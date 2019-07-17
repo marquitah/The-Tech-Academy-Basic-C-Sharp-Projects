82535ecdc3bae84ca24868b8ff541f3e550a893a
Marquita <Marquita.dec@gmail.com>
2019-07-17 14:18:27 -0600

    Add files via upload

---
+++ ClassMethodDrill/ClassMethodDrill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method of the console app, instantiate the class.
+
+            VoidClass void1 = new VoidClass();
+
+            //3. Call the method in the class, passing in two numbers.
+
+            void1.voidMethod(3,5);
+
+            //4. Call the method in the class, specifying the parameters by name.
+
+            void1.voidMethod(a:3, b:5);
+
+            //Console.WriteLine(5);
+            //Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ ClassMethodDrill/ClassMethodDrill/VoidClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDrill
+{
+    class VoidClass
+    {
+        //1. Create a class. In that class, create a void method that takes two integers as parameters. 
+        //Have the method do a math operation on the first integer and display the second integer to the screen.
+
+        public void voidMethod(int a, int b)
+        {
+            int output = a + 10;
+            {
+                Console.WriteLine(b + ", " + output);
+                Console.ReadLine();
+            }
+            
+        }
+     
+    }
+    
+
+}
