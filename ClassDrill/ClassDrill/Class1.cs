7ce3dd24fd3a52a6449ae7215b3905758f4b993a
Marquita <Marquita.dec@gmail.com>
2019-07-17 11:50:39 -0600

    Add files via upload

---
+++ ClassDrill/ClassDrill/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    //1. Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. 
+    //The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
+    public class Class1
+    {
+
+       
+        
+
+            public int functionOne(int a, int b)
+            {
+            return a + b;
+            }
+            
+            public int functionTwo(int a, int b)
+            {
+            return a * b;
+            }
+
+            public int functionThree(int a, int b)
+            {
+            return a / b;
+            }
+
+        
+
+    }
+}
---
+++ ClassDrill/ClassDrill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            //2. In the Main() program, ask the user what number they want to do the math operations on.
+
+            Console.WriteLine("Choose a number and I will add, multiply, & divide it by 2.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+
+            //3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+
+            Class1 x = new Class1();
+            int a = x.functionOne(input, 2);
+            int b = x.functionTwo(input, 2);
+            int c = x.functionThree(input, 2);
+
+            Console.WriteLine("Your number added, multiplied, & divided by 2 is " + a + ", " + b + ", " + c + ".");
+            Console.ReadLine();
+        }
+
+
+    }
+}
