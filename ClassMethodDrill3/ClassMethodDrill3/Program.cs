38a60d8d534afaa8b987da4874f74b67acd43656
Marquita <Marquita.dec@gmail.com>
2019-07-18 13:55:59 -0600

    Add files via upload

--- ClassMethodDrill3/ClassMethodDrill3/Program.cs
+++ ClassMethodDrill3/ClassMethodDrill3/Program.cs
@@ -11,17 +11,43 @@ namespace ClassMethodDrill3
         static void Main(string[] args)
         {
             //2. In the Main() method of the console app, instantiate the class.
-
             TwoParameters twoParameters = new TwoParameters();
 
+            // 3.Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
 
-            //4. Call the method in the class, passing in the one or two numbers entered.
+            Console.WriteLine("Enter in first number.");
+           int a = Convert.ToInt32(Console.ReadLine());
 
-           
+            Console.WriteLine("Enter in another number (if you want to, don't have to).");
+            string b = Console.ReadLine();
+
+            bool answer = string.IsNullOrEmpty(b);
+
+
+            if (answer == true)
+            {
+               
+                int c = twoParameters.parametersTwo(a);
+                Console.WriteLine("The number or numbers you entered equals " + c + ".");
+                Console.ReadLine();
+          
+            }
+            else if (answer == false)
+            {
+                int c = twoParameters.parametersTwo(a, Convert.ToInt32(b));
+                Console.WriteLine("The number or numbers you entered equals " + c + ".");
+                Console.ReadLine();
+               
+            }
+
+         //4. Call the method in the class, passing in the one or two numbers entered.
+
+
             twoParameters.parametersTwo(a: 0);
 
-            //5. Try various combinations of numbers on the code, including having no second number.
-            //input: 
+           //5. Try various combinations of numbers on the code, including having no second number.
+            //input 1st number: 5, input 2nd number: 3, output: 8
+            //input 1st number: 8, input 2nd number: (blank), output: 8
 
         }
     }
--- ClassMethodDrill3/ClassMethodDrill3/TwoParameters.cs
+++ ClassMethodDrill3/ClassMethodDrill3/TwoParameters.cs
@@ -10,35 +10,25 @@ namespace ClassMethodDrill3
     {
         //1. Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. 
         //Have the method do a math operation and return a integer result.
-        //3. Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
-        
+        public int parametersTwo(int a = 0, int b = 0)
 
-        
-
-        public int parametersTwo(int a, int b = 0)
         {
+            return a + b;
+
+
+        }
+
+    }
+}
             
             
-            Console.WriteLine("Enter in first number.");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter in another number (if you want to, don't have to).");
-            b = Convert.ToInt32(Console.ReadLine());
-            int c = a + b;
-            if (b == 0)
-            {
-                c = a + 0; 
-            }
-            
-            Console.WriteLine("The number or numbers you entered equals " + c + ".");
-            Console.ReadLine();
-            return c;
 
             
-        }
+        
 
        
 
         
 
-    }
-}
+    
+
