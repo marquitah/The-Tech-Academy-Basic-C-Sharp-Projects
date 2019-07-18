bdcc6b7370564559bf1686cf5478a8b99038685d
Marquita <Marquita.dec@gmail.com>
2019-07-18 09:04:09 -0600

    Add files via upload

---
+++ ClassMethodDrill2/ClassMethodDrill2/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDrill2
+{
+    //1. Create a class. In that class, create a method that will take in an integer, do a math operation to it and then return the answer as an integer.
+    class Class1
+    {
+        public int mathOne(int x)
+        {
+            int output = x + 10;
+            Console.WriteLine(output);
+            Console.ReadLine();
+            return output;
+        }
+
+        //3. Add a second method to the class, with the same name, that will take in a decimal, do a different math operation to it and then return the answer as an integer.
+
+        public int mathOne(decimal y)
+        {
+            decimal answer = y * 20;
+            {
+                Console.WriteLine(answer);
+                int newAnswer = Convert.ToInt32(answer);
+                Console.ReadLine();
+                return newAnswer;
+            }
+
+        }
+
+        //5. Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it and 
+        //then return the answer as an integer.
+
+        public int mathOne(string a)
+        {
+            int newA = Convert.ToInt32(a);
+            int newAnswerTwo = newA + 50;
+            Console.WriteLine(newAnswerTwo);
+            Console.ReadLine();
+            return newAnswerTwo;
+        }
+        
+    }
+}
---
+++ ClassMethodDrill2/ClassMethodDrill2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method of the console app, instantiate the class and call the one method, passing in an integer, such as 12. Then display the result to the screen.
+
+            Class1 math1 = new Class1();
+            math1.mathOne(12);
+
+
+
+            //4. In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
+
+            Class1 math2 = new Class1();
+            math2.mathOne(20.5m);
+
+
+            //6. In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. 
+            //Display the result to the screen.
+
+            Class1 math3 = new Class1();
+            math3.mathOne("10");
+
+        }
+    }
+}
