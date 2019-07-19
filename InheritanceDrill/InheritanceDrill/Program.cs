7f27c6828f806c9bd408be65d8e897b92b8d2d57
Marquita <Marquita.dec@gmail.com>
2019-07-19 10:32:02 -0600

    Add files via upload

---
+++ InheritanceDrill/InheritanceDrill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //4. Inside of the Main method, instantiate and initialize an Employee object with a first name of "Sample" and a last name of "Student".
+
+            Employee name = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+            };
+            //5. Call the superclass method SayName() on the Employee object.
+
+            name.SayName();
+        }
+    }
+}
