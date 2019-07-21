eab9e800028ff35d671b4d5ce3062fa54c987b2f
Marquita <Marquita.dec@gmail.com>
2019-07-21 12:38:03 -0600

    Add files via upload

---
+++ AbstractDrill/AbstractDrill/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDrill
+{//3. Create another class called Employee and have it inherit from the Person class.
+    public class Employee : Person 
+    {
+        //4. Implement the SayName() method inside of the Employee class.
+
+        public override void SayName()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ AbstractDrill/AbstractDrill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+            //5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object.
+
+            Employee e = new Employee()
+            {
+                firstName = "Sample",
+                lastName = "Student",
+            };
+           
+           
+           
+            e.SayName();
+
+        }
+    }
+}
