d6f7305465a45cfdf8df5035eece60e954f9ac22
Marquita <Marquita.dec@gmail.com>
2019-07-22 14:37:07 -0600

    Add files via upload

---
+++ GenericDrill/GenericDrill/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDrill
+{//1. Make the Employee class take a generic type parameter.
+    class Employee<T>
+    {
+        //2. Add a property to the Employee class called "things" and have
+        //its data type be a generic list matching the generic type of the class.
+
+        public List<T> Things { get; set; } = new List<T>();
+
+    }
+
+
+}
+
---
+++ GenericDrill/GenericDrill/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Instantiate an Employee object with type "string" as 
+            //its generic parameter. Assign a list of strings as the property value of Things.
+
+            Employee<string> employee = new Employee<string>();
+            {
+                employee.Things.Add("boat");
+                employee.Things.Add("car");
+                employee.Things.Add("house");
+
+            }
+            //4. Instantiate an Employee object with type "int" as its generic parameter. 
+            //Assign a list of integers as the property value of Things.
+
+            Employee<int> employee1 = new Employee<int>();
+            {
+                employee1.Things.Add(10);
+                employee1.Things.Add(32);
+                employee1.Things.Add(94);
+            }
+            //5. Create a loop that prints all of the Things to the Console.
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+                Console.ReadLine();
+            }
+            foreach (int things in employee1.Things)
+            {
+                Console.WriteLine(things);
+                Console.ReadLine();
+            }
+
+
+            
+          
+           
+        }
+    }
+}
