8f3eaeb7707e4b9658838a17ce3591476a3faaee
Marquita <Marquita.dec@gmail.com>
2019-07-24 10:26:50 -0600

    Add files via upload

---
+++ TextFileLog/TextFileLog/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextFileLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in a number.");
+            string numberEntered = Convert.ToString(Console.ReadLine());
+
+            string Text = numberEntered;
+            File.WriteAllText(@"C:\Users\Marquita\Documents\Log.txt", Text);
+
+            Console.WriteLine("The number you entered was " + Text + ".");
+            Console.ReadLine();
+            
+            
+
+        }
+    }
+}
