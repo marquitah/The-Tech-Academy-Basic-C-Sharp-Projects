05e4a2b24b3422c201fea96c09b902d82f33c1da
Marquita <Marquita.dec@gmail.com>
2019-07-24 11:20:27 -0600

    Add files via upload

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The date and time is " + DateTime.Now + ". Please enter in a number.");
+            string numberEntered = Convert.ToString(Console.ReadLine());
+            double newNumber = Convert.ToDouble(numberEntered);
+
+
+            DateTime dt = DateTime.Now;
+            DateTime newTime = dt.AddHours(newNumber);
+
+            
+            
+
+            Console.WriteLine(newTime);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
