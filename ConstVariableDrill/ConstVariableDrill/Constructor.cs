da3f6c41556c2f947bfb58fdc32d6b97b20046ee
Marquita <Marquita.dec@gmail.com>
2019-07-24 14:18:40 -0600

    Add files via upload

---
+++ ConstVariableDrill/ConstVariableDrill/Constructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVariableDrill
+{
+    public class Constructor
+    {
+        public Constructor(string address) : this (address, "CO")
+        {
+
+        }
+        public Constructor(string address, string state)
+        {
+
+        }
+
+    }
+}
